d5ff56629b2a1bb998ef48fcfd10c4c01b21c9b4
weijun620 <1092751942@qq.com>
2021-02-01 00:02:44 +0800

    123

---
+++ src/index.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square
+    value={this.props.squares[i]}
+    onClick={() => this.props.onClick(i)}
+  />
+  }
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      history:[
+        {squares:Array(9).fill(null)}
+      ],
+      xIsNext:true,
+    }
+  }
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(let i=0;i<lines.length;i++){
+      let [a,b,c]=lines[i];
+      if(squares[a]&&squares[a]===squares[b]&&squares[b]===squares[c]){
+        return squares[a]
+      }
+    }
+    return null;
+  }
+  handleClick(i){
+    console.log(i)
+    let history=this.state.history;
+    let current = history[history.length - 1];
+    let squares = current.squares;
+    if(this.calculateWinner(squares)||squares[i]){
+      return;
+    }
+    squares[i]=this.state.xIsNext?'X':'O';
+    this.setState({
+      history:history.concat([{
+        squares:squares
+      }]),
+      xIsNext:!this.state.xIsNext
+    })
+  }
+  render() {
+    let history=this.state.history;
+    let current=history[history.length-1];
+    let isWinner=this.calculateWinner(current.squares);
+    let status="";
+    if(isWinner){
+      status=`胜者: ${isWinner}`;
+    }else{
+      status=`下一位: ${this.state.xIsNext?'X':'O'}`;
+    }
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i=>this.handleClick(i)}/>
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
